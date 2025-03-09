dee7ac207e2fea3e10321da4eb294d7532391fbe
Anna <AnnaAndresen@outlook.com>
2025-03-09 16:22:47 -0700

    updates to come of the methods and viz example

--- src/data_reading.py
+++
@@ -1,1 +0,0 @@
-from src.db_handler import DatabaseHandler
--- src/db_handler.py
+++ src/db_handler.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.engine.base import Connection, Engine
 from sqlalchemy.sql.schema import Table as SQLTable
 from sqlalchemy import text, select
+from geoalchemy2 import Geometry
 import geopandas as gpd
 import os
 import pandas as pd
@@ -86,7 +87,7 @@ class DatabaseHandler:
             rows = result.fetchall()
         return pd.DataFrame(rows, columns=sql_table.columns.keys())
 
-    def execute_sql(self, statement: str) -> List[Any]:
+    def execute_custom_sql(self, statement: str) -> List[Any]:
         """
         Execute an SQL statement and return the results.
 
@@ -100,7 +101,7 @@ class DatabaseHandler:
             print(f"An error occurred: {e}")
             return []
 
-    def execute_geo_sql(self, statement: str) -> gpd.GeoDataFrame:
+    def execute_custom_geo_sql(self, statement: str) -> gpd.GeoDataFrame:
         """
         Execute an SQL statement and return the results as a GeoDataFrame.
 
@@ -111,6 +112,44 @@ class DatabaseHandler:
         gdf = gpd.read_postgis(statement, con=self.engine)
         # return the GeoDataFrame
         return gdf
+
+    def get_geo_table_data(
+        self, table_name: str, geom_col: str = None, where_clause: str = None
+    ) -> gpd.GeoDataFrame:
+        """
+        Get a specific table by name and return a geopandas dataframe.
+
+        :param table_name: The name of the table
+        :param geom_col: The geometry column to use
+        :param where_clause: The SQL WHERE clause to filter the data
+        :return: The GeoDataFrame
+        """
+        # refreshing the connection
+        self.connection = self.engine.connect()
+        # Get the table
+        table = self.get_table(table_name)
+        # Find all geometry columns
+        geom_cols = [
+            column.name for column in table.columns if isinstance(column.type, Geometry)
+        ]
+        # checking to see if there are no geometry columns:
+        if not geom_cols:
+            raise ValueError(f"No geometry columns found in table '{table_name}'")
+        # getting the local variable geom_col confirmed it's an option and can select
+        if geom_col is None:
+            # Use the first geometry column if none is specified
+            geom_col = geom_cols[0]
+        elif geom_col not in geom_cols:
+            raise ValueError(
+                f"Specified geometry column '{geom_col}' not found in table '{table_name}'. Available geometry columns: {geom_cols}"
+            )
+        # Construct the SQL query
+        query = f"SELECT * FROM {table_name}"
+        if where_clause:
+            query += f" WHERE {where_clause}"
+
+        # return the geopandas dataframe
+        return gpd.read_postgis(query, con=self.connection, geom_col=geom_col)
 
     def close_connection(self) -> None:
         """
